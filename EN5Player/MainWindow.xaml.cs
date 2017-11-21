3d88b1d9fdc1e2a8c38352f55ad8266de8e9f989
Iron <iron.yehong@outlook.com>
2017-11-21 18:53:14 +0800

    Initialize project with a WPF application.

---
+++ EN5Player/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace EN5Player
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
